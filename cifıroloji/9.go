c36d35246b562bb00cd33b1b85f31721b5768e81
Tarık Özkanlı <tozkanli2023@gmail.com>
2017-02-21 20:31:56 +0200

    go is a programming language

---
+++ cifıroloji/9.go
@@ -0,0 +1,9 @@
+Elimden düşen papatyanın üstünde
+Rüzgardan saçları dalgalanan sevgilimi
+Gördüğüm gün kimseye
+Söylemedim.
+Ben de döndüm
+Aynada uzaklaşan yüzümü seyrettim.
+Kimbilir, belki bir gün
+Saçlarımız yine beraber
+Uçuşur...
